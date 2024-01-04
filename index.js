ba91f8a2ed088d7b3f9a5fb1008d75e6f021993c
ochelle <alexizochelle@gmail.com>
2024-01-04 12:40:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Alexander Ochelle is the champion";
+
+document.body.appendChild(newHeader);
+
